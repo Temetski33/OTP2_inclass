44db838765f1d5d2df2641b7f9e8d8a4797bc6cb
Temetski33 <teemu.laasio@gmail.com>
2026-03-23 23:47:38 +0200

    Add shopping cart functionality

--- src/main/java/ShoppingCart.java
+++ src/main/java/ShoppingCart.java
@@ -18,6 +18,20 @@ public class ShoppingCart {
 
         ResourceBundle rb = ResourceBundle.getBundle("MessagesBundle", locale);
         System.out.println(rb.getString("prompt1"));
+        double a = input.nextDouble();
         System.out.println(rb.getString("prompt2"));
+        double b = input.nextDouble();
+        double summa = addMe(a, b);
+        double erotus = subMe(a, b);
+        System.out.println(rb.getString("sum") + " " + summa);
+        System.out.println(rb.getString("subtract") + " " + erotus);
+    }
+
+    public static double addMe(double a, double b) {
+        return a + b;
+    }
+
+    public static double subMe(double a, double b) {
+        return a - b;
     }
 }
